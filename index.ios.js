d02849eeaaab78197ccdec2c9b5772860cef619b
Mali Michael <malimccalla@gmail.com>
2016-10-06 20:24:34 +0100

    fixes camera privacy concerns

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,64 @@
-import { AppRegistry } from 'react-native';
-import App from './src/App';
+'use strict';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+import Camera from 'react-native-camera';
 
+class BadInstagramCloneApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          camptureTarget={Camera.constants.CaptureTarget.temp}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
+      </View>
+    );
+  }
 
-AppRegistry.registerComponent('MapApp', () => App);
+  takePicture() {
+    this.camera.capture()
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
+
+AppRegistry.registerComponent('MapApp', () => BadInstagramCloneApp);
+
+// import { AppRegistry } from 'react-native';
+// import App from './src/App';
+//
+//
+// AppRegistry.registerComponent('MapApp', () => App);
